31095051b2f4d8ffd4e9c1c2afb7380cdb602470
Ishaandeepak10 <85345071+Ishaandeepak10@users.noreply.github.com>
2022-01-23 18:44:28 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,6 @@
+<script>
+    var cursor=document.getElementByIdaa("cursor");
+        document.onmousemove=function(e)
+        cursor.style.left=e.pageX + "px";
+        cursor.style.top=e.pageY + "px";
+    </script>
